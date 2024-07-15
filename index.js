6092a10453bdd34768672d683255e0019104c3f5
Jae Jeong <jeongjj29@gmail.com>
2024-07-15 14:34:54 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    return Math.abs(street - 42);
+}
+
+function distanceFromHqInFeet(street) {
+    return distanceFromHqInBlocks(street) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs(start - end) * 264;
+}
+
+function calculatesFarePrice (start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance > 2500) {
+        return "cannot travel that far";
+    } else if (distance > 2000) {
+        return 25;
+    } else if (distance > 400) {
+        return (distance - 400) * 0.02;
+    } else {
+        return 0;
+    }
+}
